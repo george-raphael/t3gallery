621bc59b33ca602098cbf64ae14fb149834becdb
George <georgie817@gmail.com>
2024-08-16 10:52:45 +0300

    database linking to UI completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,22 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/8e54231f-958f-40c8-9707-9fb417a4a27e-q9z07t.png",
-  "https://utfs.io/f/6cec69b4-7a97-480e-9ef1-6ec54eba3ed7-82kk33.png",
-  "https://utfs.io/f/174a3a7c-52bd-4af2-bcd6-b8a95617adeb-j3g0yd.png",
-  "https://utfs.io/f/87cbc836-fc81-43e6-adcd-7e96ea9dcdd1-bbuu15.png",
-  "https://utfs.io/f/0cedf047-207c-4c30-a72c-af574f347f3c-b16ii1.png",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id : index,
-  url: url
-}))
 
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
+  const mockImages = await db.query.images.findMany({
+    orderBy: (model, { desc}) => desc(model.name)
+  });
 
   return (
     <main className="min-h-screen bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="flex flex-wrap gap-3 p-3">
-      {posts.map((post) => (<div key={post.id}>{post.name}</div>))}
       {
         mockImages.map((image) => (
           <div key={image.id} className="rounded-lg overflow-hidden shrink ">
             <a href={`/image/${image.id}`}>
               <img src={image.url} alt="image" className="w-64 rounded-lg" />
+              <div>{image.name}</div>
             </a>
           </div>
         ))
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -18,11 +18,12 @@ import {
  */
 export const createTable = pgTableCreator((name) => `t3gallery_${name}`);
 
-export const posts = createTable(
-  "post",
+export const images = createTable(
+  "images",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
+    url: varchar("url", { length: 1024 }).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
@@ -30,7 +31,7 @@ export const posts = createTable(
       () => new Date()
     ),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
+  (table) => ({
+    nameIndex: index("name_idx").on(table.name),
   })
 );
